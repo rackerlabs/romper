4dfe8c639da750bf71e2d64d7a00cc921f419d90
Jim Baker <jim.baker@rackspace.com>
2013-11-08 11:18:05 -0800

    Oops, forgotten commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "romper",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ["clamp>=0.2"],
+    clamp = ["romper.topology"],
+)
